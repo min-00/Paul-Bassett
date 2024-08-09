80cb38eee2a8625581c6d9a34530c8e277010408
min-00 <rldkatjd3579@gmail.com>
2024-08-09 16:59:42 +0900

    0809(마이페이지)

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
+    <div class="header">
+      <p class="menu_title">마이페이지</p>
+    </div>
     <v-row>
-      <v-col cols="12" sm="6">
-        <v-card>
-          <v-card-title>
-            <span class="headline">마이페이지</span>
-          </v-card-title>
+      <v-col cols="10" offset="1">
+        <v-card class="mt-4">
           <v-card-subtitle>
             <v-avatar size="40">
               <v-icon>mdi-account</v-icon>
@@ -27,10 +27,32 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="6">
-        <v-card>
-            <router-link to="/Orderhistory"> 주문 내역 </router-link>
-        </v-card>
+      <v-col cols="12">
+        <router-link to="/Orderhistory" class="pa-4 menu_list">
+          <h4>주문 내역</h4>
+          <v-icon>arrow_forward_ios</v-icon>
+        </router-link>
+        <v-divider />
+        <router-link to="/Menu" class="pa-4 menu_list">
+          <h4>픽업 주문하기</h4>
+          <v-icon>arrow_forward_ios</v-icon>
+        </router-link>
+        <v-divider />
+        <router-link to="/Orderhistory" class="pa-4 menu_list">
+          <h4>선물하기 및 내역</h4>
+          <v-icon>arrow_forward_ios</v-icon>
+        </router-link>
+        <v-divider />
+        <router-link to="/Orderhistory" class="pa-4 menu_list">
+          <h4>FAQ or 자주 묻는 질문</h4>
+          <v-icon>arrow_forward_ios</v-icon>
+        </router-link>
+        <v-divider />
+        <router-link to="/Orderhistory" class="pa-4 menu_list">
+          <h4>앱설정</h4>
+          <v-icon>arrow_forward_ios</v-icon>
+        </router-link>
+        <v-divider />
       </v-col>
     </v-row>
   </v-container>
@@ -40,17 +62,34 @@
 export default {
   data() {
     return {
-      userInfo: JSON.parse(localStorage.getItem('loggedInUser')) || {},
+      userInfo: JSON.parse(localStorage.getItem("loggedInUser")) || {}
     };
   },
   methods: {
     logout() {
       // 로컬스토리지에서 사용자 정보 삭제
-      localStorage.removeItem('loggedInUser');
+      localStorage.removeItem("loggedInUser");
 
-      this.$router.push('/Login');
+      this.$router.push("/Login");
     }
   }
 };
 </script>
 
+<style>
+.menu_list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-decoration: none;
+}
+
+h4{
+  color:#1E1E1E;
+}
+
+.v-card__actions{
+  justify-content: space-around;
+  padding-bottom: 14px;
+}
+</style>
